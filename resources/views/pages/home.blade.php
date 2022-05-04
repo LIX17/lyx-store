852e0781f5cb7e5b2605ded7d13b23d83b1d9a79
LIX17 <felixlix17@gmail.com>
2022-05-05 02:37:12 +0700

    integration home product list

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -95,206 +95,40 @@
             </div>
             </div>
             <div class="row">
-            <div
-                class="col-6 col-md-4 col-lg-3"
-                data-aos="fade-up"
-                data-aos-delay="100"
-            >
-                <a href="/details.html" class="component-products d-block">
-                <div class="card" style="border-radius: 12px">
-                    <div class="products-thumbnail">
-                    <div
-                        class="products-image"
-                        style="
-                        background-image: url('/images/Product images/watch.jpg');
-                        "
-                    ></div>
+                @php $increment=0; @endphp
+                @forelse ($products as $row)
+                    <div class="col-6 col-md-4 col-lg-3" data-aos="fade-up" data-aos-delay="{{ $increment+=100 }}">
+                        <a href="{{ route('detail', $row->slug) }}" class="component-products d-block">
+                        <div class="card" style="border-radius: 12px">
+                            <div class="products-thumbnail">
+                            <div
+                                class="products-image"
+                                style="
+                                    @if($row->galleries->count())
+                                        background-image: url('{{ Storage::url($row->galleries->first()->url) }}');
+                                        background-size: 260px 140px;
+                                    @else
+                                        background-image: #eee;
+                                    @endif                                
+                                "
+                            ></div>
+                            </div>
+                            <div
+                            class="card-body"
+                            style="background-color: #f9f3df !important"
+                            >
+                            <div class="products-text">{{ $row->name }}</div>
+                            <div class="products-price">{{ $row->price }}</div>
+                            </div>
+                        </div>
+                        </a>
                     </div>
-                    <div
-                    class="card-body"
-                    style="background-color: #f9f3df !important"
-                    >
-                    <div class="products-text">Apple Watch</div>
-                    <div class="products-price">$400</div>
+                @empty
+                    <div class="col-12 text-center py-5" data-aos="fade-up" data-aos-delay="100">
+                        Products Are Not Found
                     </div>
-                </div>
-                </a>
-            </div>
-            <div
-                class="col-6 col-md-4 col-lg-3"
-                data-aos="fade-up"
-                data-aos-delay="200"
-            >
-                <a href="/details.html" class="component-products d-block">
-                <div class="card" style="border-radius: 12px">
-                    <div class="products-thumbnail">
-                    <div
-                        class="products-image"
-                        style="
-                        background-image: url('/images/Product images/orange-shoes.jpg');
-                        "
-                    ></div>
-                    </div>
-                    <div
-                    class="card-body"
-                    style="background-color: #f9f3df !important"
-                    >
-                    <div class="products-text">Orenji</div>
-                    <div class="products-price">$250</div>
-                    </div>
-                </div>
-                </a>
-            </div>
-            <div
-                class="col-6 col-md-4 col-lg-3"
-                data-aos="fade-up"
-                data-aos-delay="300"
-            >
-                <a href="/details.html" class="component-products d-block">
-                <div class="card" style="border-radius: 12px">
-                    <div class="products-thumbnail">
-                    <div
-                        class="products-image"
-                        style="
-                        background-image: url('/images/Product images/drone.jpg');
-                        "
-                    ></div>
-                    </div>
-                    <div
-                    class="card-body"
-                    style="background-color: #f9f3df !important"
-                    >
-                    <div class="products-text">Baling-baling besi</div>
-                    <div class="products-price">$4000</div>
-                    </div>
-                </div>
-                </a>
-            </div>
-            <div
-                class="col-6 col-md-4 col-lg-3"
-                data-aos="fade-up"
-                data-aos-delay="400"
-            >
-                <a href="/details.html" class="component-products d-block">
-                <div class="card" style="border-radius: 12px">
-                    <div class="products-thumbnail">
-                    <div
-                        class="products-image"
-                        style="
-                        background-image: url('/images/Product images/dolls.jpg');
-                        "
-                    ></div>
-                    </div>
-                    <div
-                    class="card-body"
-                    style="background-color: #f9f3df !important"
-                    >
-                    <div class="products-text">Teddy Bear</div>
-                    <div class="products-price">$20</div>
-                    </div>
-                </div>
-                </a>
-            </div>
-            <div
-                class="col-6 col-md-4 col-lg-3"
-                data-aos="fade-up"
-                data-aos-delay="500"
-            >
-                <a href="/details.html" class="component-products d-block">
-                <div class="card" style="border-radius: 12px">
-                    <div class="products-thumbnail">
-                    <div
-                        class="products-image"
-                        style="
-                        background-image: url('/images/Product images/sofa.jpg');
-                        "
-                    ></div>
-                    </div>
-                    <div
-                    class="card-body"
-                    style="background-color: #f9f3df !important"
-                    >
-                    <div class="products-text">Soft-a</div>
-                    <div class="products-price">$600</div>
-                    </div>
-                </div>
-                </a>
-            </div>
-            <div
-                class="col-6 col-md-4 col-lg-3"
-                data-aos="fade-up"
-                data-aos-delay="600"
-            >
-                <a href="/details.html" class="component-products d-block">
-                <div class="card" style="border-radius: 12px">
-                    <div class="products-thumbnail">
-                    <div
-                        class="products-image"
-                        style="
-                        background-image: url('/images/Product images/powder.jpg');
-                        "
-                    ></div>
-                    </div>
-                    <div
-                    class="card-body"
-                    style="background-color: #f9f3df !important"
-                    >
-                    <div class="products-text">Cocoa Powder</div>
-                    <div class="products-price">$30</div>
-                    </div>
-                </div>
-                </a>
-            </div>
-            <div
-                class="col-6 col-md-4 col-lg-3"
-                data-aos="fade-up"
-                data-aos-delay="700"
-            >
-                <a href="/details.html" class="component-products d-block">
-                <div class="card" style="border-radius: 12px">
-                    <div class="products-thumbnail">
-                    <div
-                        class="products-image"
-                        style="
-                        background-image: url('/images/Product images/placemat.jpg');
-                        "
-                    ></div>
-                    </div>
-                    <div
-                    class="card-body"
-                    style="background-color: #f9f3df !important"
-                    >
-                    <div class="products-text">Placemat</div>
-                    <div class="products-price">$10</div>
-                    </div>
-                </div>
-                </a>
-            </div>
-            <div
-                class="col-6 col-md-4 col-lg-3"
-                data-aos="fade-up"
-                data-aos-delay="800"
-            >
-                <a href="/details.html" class="component-products d-block">
-                <div class="card" style="border-radius: 12px">
-                    <div class="products-thumbnail">
-                    <div
-                        class="products-image"
-                        style="
-                        background-image: url('/images/Product images/black-shoes.jpg');
-                        "
-                    ></div>
-                    </div>
-                    <div
-                    class="card-body"
-                    style="background-color: #f9f3df !important"
-                    >
-                    <div class="products-text">Air Nike</div>
-                    <div class="products-price">$3000</div>
-                    </div>
-                </div>
-                </a>
-            </div>
+                @endforelse            
+            
             </div>
         </div>
         </section>
